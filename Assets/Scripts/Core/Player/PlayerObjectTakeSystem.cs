af9affffa82ea35b7f2347e66e79cb4651dcf9ca
Hezekiel112 <127784200+hezekiel112@users.noreply.github.com>
2024-08-26 16:25:05 +0200

    correctif inventaire optimalStackSize

--- Assets/Scripts/Core/Player/PlayerObjectTakeSystem.cs
+++ Assets/Scripts/Core/Player/PlayerObjectTakeSystem.cs
@@ -85,20 +85,27 @@ public class PlayerObjectTakeSystem : MonoBehaviour
                         else if ((slot.Stack + 1) == slot.GetItem().MaxStackSize) {
                             slot.Stack++;
 
+                            PlayerHUD.OnItemAdded(slot.SlotID, slot.Stack);
+
                             if (_playerInventory.GetSlotWithItem(item, out var slotItem)) {
-                                if ((slotItem.Stack + item.Item.DefaultStackSize - 1) <= slotItem.GetItem().MaxStackSize) {
-                                    slotItem.Stack += item.Item.DefaultStackSize - 1;
+                                int optimalStackSize = item.Item.DefaultStackSize + slotItem.Stack - 1;
+
+                                print($"{nameof(optimalStackSize)} : {optimalStackSize}");
+
+                                if (optimalStackSize <= slotItem.Stack) {
+                                    slotItem.Stack += optimalStackSize;
+
+                                    StartCoroutine(PlayerHUD.OnDisplayEnter(pickedUpItem: item, count: optimalStackSize));
                                     PlayerHUD.OnItemAdded(slotItem.SlotID, slotItem.Stack);
                                 }
                             }
                             else if (AddFreeSlot(item, out var newFreeSlot)) {
                                 newFreeSlot.AddItemToSlot(item, item.Item.DefaultStackSize - 1);
+
                                 StartCoroutine(PlayerHUD.OnDisplayEnter(pickedUpItem: item, count: item.Item.DefaultStackSize));
                             } else {
                                 StartCoroutine(PlayerHUD.OnDisplayEnter("inventory is full !", true));
                             }
-
-                            PlayerHUD.OnItemAdded(slot.SlotID, slot.Stack);
                         }
                     } else {
 
